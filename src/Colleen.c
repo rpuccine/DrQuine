185f70e227ff40a27763c7f9b7c4891ceb0f5c1a
Remi PUCCINELLI <rpuccine@e1r5p3.42.fr>
2016-09-13 17:46:44 +0200

    Colleen done

--- src/Colleen.c
+++ src/Colleen.c
@@ -2,6 +2,6 @@
 /* coucou */
 int main()
 {
-char *str = "#include <stdio.h>%c/* coucou */%cint main()%c{%cchar *str =%c%s%c;%cprintf(str, );%c}";
-printf(str, 10, 10, 10, 10, 34, str, 34, 10, 10);
+char *str = "#include <stdio.h>%c/* coucou */%cint main()%c{%cchar *str = %c%s%c;%cprintf(str, 10, 10, 10, 10, 34, str, 34, 10, 10, 10);%c}%c";
+printf(str, 10, 10, 10, 10, 34, str, 34, 10, 10, 10);
 }
