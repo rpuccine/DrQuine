25092c4caffbfd0e460bb1bfb67c5c3d0fd93685
Remi PUCCINELLI <rpuccine@e1r5p3.42.fr>
2016-09-13 17:59:21 +0200

    fix colleen

--- src/Colleen.c
+++ src/Colleen.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 /* coucou */
+void func() {}
 int main()
 {
-char *str = "#include <stdio.h>%c/* coucou */%cint main()%c{%cchar *str = %c%s%c;%cprintf(str, 10, 10, 10, 10, 34, str, 34, 10, 10, 10);%c}%c";
-printf(str, 10, 10, 10, 10, 34, str, 34, 10, 10, 10);
+/* au revoir */
+char *str = "#include <stdio.h>%c/* coucou */%cvoid func() {}%cint main()%c{%c/* au revoir */%cchar *str = %c%s%c;%cprintf(str, 10, 10, 10, 10, 10, 10, 34, str, 34, 10, 10, 10);%c}%c";
+printf(str, 10, 10, 10, 10, 10, 10, 34, str, 34, 10, 10, 10);
 }
