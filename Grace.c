8367c80f32fa5776276b18c34988f158eb9993d1
Remi PUCCINELLI <rpuccine@e1r4p11.42.fr>
2016-09-14 16:11:01 +0200

    renamed grace_2 in grace

--- Grace.c
+++ Grace.c
@@ -1,8 +1,7 @@
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
 #include <stdio.h>
-#include <string.h>
-#define MAIN int main() { int fd;fd = open("Grace_kid.c", O_WRONLY | O_CREAT, S_IRWXU);char str[800];char *tmp = "#include <sys/types.h>%c#include <sys/stat.h>%c#include <fcntl.h>%c#include <unistd.h>%c#include <stdio.h>%c#include <string.h>%c#define MAIN int main() { int fd;fd = open(%cGrace_kid.c%c, O_WRONLY | O_CREAT, S_IRWXU);char str[800];char *tmp = %c%s%c;sprintf(str, tmp, 10, 10, 10, 10, 10, 10, 34, 34, 34, tmp, 34, 10, 10);write(fd, str, strlen(str));}%cMAIN%c";sprintf(str, tmp, 10, 10, 10, 10, 10, 10, 34, 34, 34, tmp, 34, 10, 10);write(fd, str, strlen(str));}
-MAIN
+/*
+	this is comment
+*/
+#define STR "#include <stdio.h>%c/*%c%cthis is comment%c*/%c#define STR %c%s%c%c#define MAIN() int main() {FILE *f = fopen(%cGrace_kid.c%c, %cw+%c); fprintf(f, STR, 10, 10, 9, 10, 10, 34, STR, 34, 10, 34, 34, 34, 34, 10, 10);}%cMAIN()%c"
+#define MAIN() int main() {FILE *f = fopen("Grace_kid.c", "w+"); fprintf(f, STR, 10, 10, 9, 10, 10, 34, STR, 34, 10, 34, 34, 34, 34, 10, 10);}
+MAIN()
