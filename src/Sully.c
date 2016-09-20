32735cec08ab23ded7c5dd6dcb155df4510bff78
Remi PUCCINELLI <rpuccine@e1r4p9.42.fr>
2016-09-20 18:33:32 +0200

    almost done

--- src/Sully.c
+++ src/Sully.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #define NB 5
 #define FILE_NAME(NB) Sully_##NB
 #define FILE(NB) FILE_NAME(NB)
@@ -8,13 +9,16 @@ int main()
 {
 int i = NB;
 if (i <= 0)
-	return ;
+	return (0);
 char *f_name;
+char *f_name_c;
 char *cmd;
 asprintf(&f_name, "%s", EX_TO_STR(FILE(NB)));
+asprintf(&f_name_c, "%s.c", f_name);
 asprintf(&cmd, "clang -Wall -Wextra -Werror %s.c -o %s ; ./%s", f_name, f_name, f_name);
-char *str = "#include <stdio.h>%c#define NB 5%c#define FILE_NAME(NB) Sully_##NB%c#define FILE(NB) FILE_NAME(NB)%c#define TO_STR(STR) #STR%c#define EX_TO_STR(STR) TO_STR(STR)%cint main()%c{%cint i = NB;%cif (i <= 0)%c%creturn ;%cchar *f_name;%cchar *cmd;%casprintf(&f_name, %c%%s%c, EX_TO_STR(FILE(NB)));%casprintf(&cmd, %cclang -Wall -Wextra -Werror %%s.c -o %%s ; ./%%s%c, f_name, f_name, f_name);%c%s%c;%cFILE *f = fopen(f_name, %cw+%c);%cfprintf(f, %c%%s%c, STR);%csystem(cmd);%c}%c";
-FILE *f = fopen(f_name, "w+");
-fprintf(f, "%s", HEEEEEEEEREEEEEEE);
+printf("**cmd = %s\n**f_name = %s\n**f_name_c = %s\n", cmd, f_name, f_name_c);
+char *str = "#include <stdio.h>%c#include <stdlib.h>%c#define NB %d%c#define FILE_NAME(NB) Sully_##NB%c#define FILE(NB) FILE_NAME(NB)%c#define TO_STR(STR) #STR%c#define EX_TO_STR(STR) TO_STR(STR)%cint main()%c{%cint i = NB;%cif (i <= 0)%c%creturn (0);%cchar *f_name;%cchar *f_name_c;%cchar *cmd;%casprintf(&f_name, %c%%s%c, EX_TO_STR(FILE(NB)));%casprintf(&f_name_c, %c%%s.c%c, f_name);%casprintf(&cmd, %cclang -Wall -Wextra -Werror %%s.c -o %%s ; ./%%s%c, f_name, f_name, f_name);%cchar *str = %c%s%c;%cFILE *f = fopen(f_name_c, %cw+%c);%cfprintf(f, str, 10, 10, --i, 10, 10, 10, 10, 10, 10, 10, 10, 10, 9, 10, 10, 10, 10, 34, 34, 10, 34, 34, 10, 34, 34, 10, 34, str, 34, 10, 34, 34, 10, 10, 10, 10);%csystem(cmd);%c}%c";
+FILE *f = fopen(f_name_c, "w+");
+fprintf(f, str, 10, 10, --i, 10, 10, 10, 10, 10, 10, 10, 10, 10, 9, 10, 10, 10, 10, 34, 34, 10, 34, 34, 10, 34, 34, 10, 34, str, 34, 10, 34, 34, 10, 10, 10, 10);
 system(cmd);
 }
